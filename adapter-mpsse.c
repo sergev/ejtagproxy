02aa5134319a14dc31bad813ddfe3f1271e9e443
serge@vak.ru <serge@vak.ru@5a1d93bf-838d-15af-83c7-8f9a87a3acc9>
2012-10-02 05:34:33 +0000

    Olimex ARM-USB-OCD-H adapter supported.

--- adapter-mpsse.c
+++ adapter-mpsse.c
@@ -92,6 +92,7 @@ typedef struct {
 #define OLIMEX_VID              0x15ba
 #define OLIMEX_ARM_USB_TINY     0x0004  /* ARM-USB-Tiny */
 #define OLIMEX_ARM_USB_TINY_H   0x002a	/* ARM-USB-Tiny-H */
+#define OLIMEX_ARM_USB_OCD_H    0x002b	/* ARM-USB-OCD-H */
 
 #define DP_BUSBLASTER_VID       0x0403
 #define DP_BUSBLASTER_PID       0x6010  /* Bus Blaster v2 */
@@ -797,6 +798,18 @@ adapter_t *adapter_open_mpsse (void)
                 a->led_control    = 0x0800;
                 goto found;
             }
+            if (dev->descriptor.idVendor == OLIMEX_VID &&
+                dev->descriptor.idProduct == OLIMEX_ARM_USB_OCD_H) {
+                a->adapter.name = "Olimex ARM-USB-OCD-H";
+                a->mhz = 30;
+                a->dir_control     = 0x0f10;
+                a->trst_control    = 0x0100;
+                a->trst_inverted   = 1;
+                a->sysrst_control  = 0x0200;
+                a->led_control     = 0x0800;
+                a->sysrst_inverted = 1;         /* changed for mips */
+                goto found;
+            }
             if (dev->descriptor.idVendor == DP_BUSBLASTER_VID &&
                 dev->descriptor.idProduct == DP_BUSBLASTER_PID) {
                 a->adapter.name = "Dangerous Prototypes Bus Blaster";
@@ -935,8 +948,7 @@ failed:
         {
             fprintf (stderr, "%s: invalid status = %04x\n", a->adapter.name, status);
             mpsse_reset (a, 0, 0, 0);
-            free (a);
-            return 0;
+            goto failed;
         }
 
         /* Leave it in ETAP mode. */
